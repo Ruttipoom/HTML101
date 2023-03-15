c55d1b90b0ffbc0347c2418b55f4cd62c75e0751
Ruttipoom <Ruttipoomrtp8@gmail.com>
2023-03-15 22:48:27 +0700

    Practice JS

---
+++ Javascript/test popup/function.js
@@ -0,0 +1,5 @@
+function hello() {
+    alert("HELLO,DA WORLD"); 
+    console.log ("Display message has been displayed");
+    document.getElementById("demo").innerHTML = "The button has been clicked";
+}
